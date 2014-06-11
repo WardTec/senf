47f13b07d45482cf1bd61074256d7ed91172fb27
sean <root@utexas.edu>
2014-06-11 14:49:03 -0500

    update build.sh to specify source/target 1.6

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-javac -cp lib/tika.jar senf/*.java streams/*.java streamsources/*.java seeds/*.java parsers/*.java
+javac -source 1.6 -target 1.6 -cp lib/tika.jar senf/*.java streams/*.java streamsources/*.java seeds/*.java parsers/*.java
 jar cmf manifest senf.jar senf/*.class senf/images/* streams/*.class streamsources/*.class
